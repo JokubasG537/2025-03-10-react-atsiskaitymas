baa00391f3dbcfff8fae5e049cab681aa89cad9b
JokubasG537 <jokub537@gmail.com>
2025-03-14 18:25:21 +0200

    more style

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import useFetchData from "../components/hooks/fetchData";
+import '../style/Users.scss'
 
 const Users = () => {
   const { data: usersData, loading, error } = useFetchData('http://localhost:3000/users');
@@ -13,11 +14,11 @@ const Users = () => {
   }
 
   return (
-    <div>
-      <h2>Users</h2>
-      <div>
+    <div className="users">
+      <h2>Our Users</h2>
+      <div className="users-wrapper">
         {usersData.map((user) => (
-          <div key={user.id}>
+          <div key={user.id} className="user-item">
             <Link to={`/users/${user.id}`}>
               <img src={user.profilePic} alt={`${user.name}'s profile`} className="user-pic" />
               {user.name}
